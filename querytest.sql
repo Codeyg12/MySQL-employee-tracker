f62c3a3aa394ce2c5c727e8fca9e6db874b1ec22
Codeyg12 <codey.gallup@gmail.com>
2022-08-13 17:25:26 -0600

    finished minimum requirments

--- querytest.sql
+++ querytest.sql
@@ -7,10 +7,7 @@ SELECT
     r.salary,
     CONCAT(m.first_name, ' ', m.last_name) manager
 FROM employee e
-JOIN employee m ON m.id = e.manager_id
-JOIN role r on e.role_id = r.id
+LEFT JOIN employee m ON m.id = e.manager_id
+JOIN role r ON e.role_id = r.id
 JOIN department d on d.id = r.department_id
 ORDER BY e.id
-
-
--- also not showing 'null' managers 
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+require("console.table");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3001,
+  port: 3306,
   user: "root",
   password: "mypass",
   database: "company_db",
 });
 
 connection.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Now connected");
+  if (err) throw err;
+  console.log(`Now connected as ${connection.threadId}`);
   startQuestions();
 });
 
@@ -64,37 +63,145 @@ startQuestions = () => {
 };
 
 viewAllEmployees = () => {
-
-}
+  connection.query(
+    `SELECT e.id, e.first_name, e.last_name, r.title, d.name, r.salary, CONCAT(m.first_name, ' ', m.last_name) manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN role r ON e.role_id = r.id JOIN department d on d.id = r.department_id ORDER BY e.id;`,
+    (err, res) => {
+      if (err) throw err;
+      console.table(
+        "\n------------------------------------------------------------------------------------", res, "\n------------------------------------------------------------------------------------"
+      );
+      startQuestions();
+    }
+  );
+};
 
 addEmployee = () => {
-
-}
+  connection.query(`SELECT * FROM role;`, (err, res) => {
+    if (err) throw err;
+  });
+  let roles = res.map((role) => ({ name: role.title, value: role.id }));
+  connection.query(`SELECT * FROM employee;`, (err, res) => {
+    if (err) throw err;
+  });
+  let employees = res.map((employee) => ({
+    name: employee.first_name + " " + employee.last_name,
+    value: employee.id,
+  }));
+  inquirer.prompt([
+    {
+      name: "firstName",
+      type: "input",
+      message: "What is the employee's first name?",
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "What is the employee's last name?",
+    },
+    {
+      name: "employeeNewRole",
+      type: "list",
+      message: "What is the employee's role",
+      choices: roles,
+    },
+    {
+      name: "employeeNewManager",
+      type: "list",
+      message: "Who is the employee's manager?",
+      choices: employees,
+    },
+  ]);
+};
 
 viewAllRoles = () => {
-    connection.query(`SELECT d.id, r.title, d.name, r.salary FROM department d JOIN role r ON d.id = r.department_id ORDER BY d.id;`, (err, res) => {
-        if (err) {
-            throw err
-        }
-        console.log('\n---------------------' + res + '\n---------------------')
-        startQuestions()
-    })
-}
+  connection.query(
+    `SELECT d.id, r.title, d.name, r.salary FROM department d JOIN role r ON d.id = r.department_id ORDER BY d.id;`,
+    (err, res) => {
+      if (err) throw err;
+      console.table(
+        "\n------------------------------------------", res, "\n------------------------------------------"
+      );
+      startQuestions();
+    }
+  );
+};
 
 addRole = () => {
-
-}
+  connection.query(`SELECT * FROM department;`, (err, res) => {
+    if (err) throw err;
+  });
+  let departments = res.map((dept) => ({ name: dept.name, value: dept.id }));
+  inquirer
+    .prompt([
+      {
+        name: "newRole",
+        type: "input",
+        message: "What new role would you like to add?",
+      },
+      {
+        name: "newSalary",
+        type: "number",
+        message: "What is the salary of the new role?",
+      },
+      {
+        name: "newRoleDept",
+        type: "list",
+        message: "Which department does the new role belong to?",
+        choices: departments,
+      },
+    ])
+    .then((response) => {
+      connection.query(
+        `INSERT INTO role SET ?`,
+        {
+          title: response.newRole,
+          salary: response.newSalary,
+          department_id: response.newRoleDept,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(`\n ${response.newRole} added to role table!`);
+          startQuestions();
+        }
+      );
+    });
+};
 
 viewAllDepartments = () => {
-    connection.query(`SELECT * FROM department ORDER BY name ASC;`, (err, res) => {
-        if (err) {
-            throw (err)
-        }
-        console.log('\n---------------------' + res + '\n---------------------')
-        startQuestions()
-    })
-}
+  connection.query(
+    `SELECT * FROM department ORDER BY name ASC;`,
+    (err, res) => {
+      if (err) throw err;
+      console.table(
+        "\n---------------------", res, "\n---------------------"
+      );
+      startQuestions();
+    }
+  );
+};
 
 addDepartment = () => {
-
-}
+  inquirer
+    .prompt([
+      {
+        name: "newDepartment",
+        type: "input",
+        message: "What department would you like to add?",
+      },
+    ])
+    .then((response) => {
+      connection.query(
+        `INSERT INTO department SET ?`,
+        {
+          name: response.newDepartment,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(
+            `\n ${response.newDepartment} added to department table!`
+          );
+          startQuestions();
+        }
+      );
+    });
+};
